75120a9ca396132fd7bd3fc0d2a8653b0fecfa7c
Justin Waltz <jwaltz001@gmail.com>
2019-10-18 13:21:02 -0500

    state working in admin components

--- b-and-h-construction/src/components/admin/administrator.js
+++ b-and-h-construction/src/components/admin/administrator.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import NewProject from './new-project.js'
 
 export default class Administrator extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: null
+		}
+	}
 	render() {
+		if(this.state.user) {
+			return(
+				<NewProject user={this.state.user}/>
+			)
+		}
 		return (
 			<div>
-				<p>You are on the Administrator component!</p>
+				<p>You are on the Administrator Login component!</p>
+
 			</div>
 		);
 	}
---
+++ b-and-h-construction/src/components/admin/new-project.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+export default class NewProject extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			projectInfo: {
+				title: '',
+				constructionType: '',
+				streetAddress: '',
+				city: '',
+				state: '',
+				zipcode: null,
+				description: '',
+				images:[],
+				postedBy: {adminID:this.props.user.id, adminUsername:this.props.user.username, date:{}}
+			}
+		}
+	}
+	render() {
+		return(
+			<div>
+				<p>Form for new project</p>
+			</div>
+		)
+	}
+}
