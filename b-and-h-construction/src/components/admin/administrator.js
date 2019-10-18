17bc2a030cd20142517a2e617bfa3d1ddf8654d5
Justin Waltz <jwaltz001@gmail.com>
2019-10-18 13:53:31 -0500

    admin login form front-end works and axios installed

--- b-and-h-construction/src/components/admin/administrator.js
+++ b-and-h-construction/src/components/admin/administrator.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
 import NewProject from './new-project.js'
+import axios from 'axios';
 
 export default class Administrator extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			user: null
+			loggedInUser: null,
+			username: '',
+			password: ''
 		}
+
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+		this.onChangePassword = this.onChangePassword.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+  	onChangeUsername(e) {
+    	this.setState({
+      		username: e.target.value
+    	});
+  	}
+
+	onChangePassword(e) {
+    	this.setState({
+      		password: e.target.value
+    	});
+  	}
+
+	onSubmit(e){
+		e.preventDefault();
+		const userLoginInfo = {
+			username: this.state.username,
+			password: this.state.password
+		};
+		console.log(userLoginInfo);
 	}
 	render() {
 		if(this.state.user) {
 			return(
 				<NewProject user={this.state.user}/>
 			)
+		}else{
+			return (
+				<div>
+					<p>You are on the Administrator Login component!</p>
+					<form onSubmit={this.onSubmit}>
+						<div className="form-group">
+							<label>Username:</label>
+							<input type="text"
+								required
+								className="form-control"
+								placeholder="Enter Administrator Username"
+								onChange={this.onChangeUsername}/>
+							<input type="password"
+								required
+								className="form-control"
+								placeholder="Enter Administrator Password"
+								onChange={this.onChangePassword}/>
+						</div>
+						<div className="form-group">
+            				<input type="submit" value="Login" className="btn btn-primary" />
+          				</div>
+					</form>
+				</div>
+			);
 		}
-		return (
-			<div>
-				<p>You are on the Administrator Login component!</p>
-
-			</div>
-		);
 	}
 }
--- b-and-h-construction/src/components/admin/new-project.js
+++ b-and-h-construction/src/components/admin/new-project.js
@@ -17,6 +17,7 @@ export default class NewProject extends Component {
 			}
 		}
 	}
+	
 	render() {
 		return(
 			<div>
